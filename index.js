b81bac38c42a1f49a869a498fa34eeab6e882045
Richard Williams <1234richardwilliams@gmail.com>
2024-10-01 21:24:50 +0100

    Added post and test

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ exports.fetch = (options) =>
   new Promise((resolve, reject) => {
     if (!options.method) options.method = 'GET';
     if (!options.port) options.port = 443;
+    if (options.data) options.method = 'POST';
     let response = '';
     const req = https.request(options, (res) => {
+      console.log(`statusCode: ${res.statusCode}`);
       res.on('data', (d) => {
         response += d.toString();
+        console.log(response);
       });
       res.on('end', () => resolve(options.type === 'txt' ? response : JSON.parse(response)));
     });
 
     req.on('error', (error) => reject(error));
 
+    if (options.data) {
+      req.write(options.data);
+    }
     req.end();
   });
--- test/test.js
+++ test/test.js
@@ -2,6 +2,16 @@ const { expect } = require('chai');
 const { fetch } = require('../index');
 
 describe('fetch', () => {
+  it.only('can make a post request', async () => {
+    const result = await fetch({
+      hostname: 'todoist.com',
+      path: '/api/v8/sync',
+      method: 'POST',
+    });
+    console.log(result);
+    expect(result).to.have.property('success').that.is.a('string');
+    expect(result.success).to.equal('true');
+  });
   it('peforms a get of JSON via https implicitly', async () => {
     const result = await fetch({ hostname: 'api.publicapis.org', path: '/entries?title=cata' });
     expect(result).to.have.property('count').that.is.a('number');
